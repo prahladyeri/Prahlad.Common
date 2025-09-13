d614e3c1f9b56015f5b5b93c558cb3142547d55a
Prahlad Yeri <prahladyeri@yahoo.com>
2025-09-13 22:32:29 +0530

    feat: Added File Helper

---
+++ FileHelper.cs
@@ -0,0 +1,51 @@
+﻿/**
+ * FileHelper.cs
+ * 
+ * @author Prahlad Yeri <prahladyeri@yahoo.com>
+ * @license MIT
+ */
+using System.IO;
+using System.Reflection;
+
+namespace Prahlad.Common
+{
+    internal static class Helper
+    {
+        internal static readonly string AppName = Assembly.GetExecutingAssembly().GetName().Name;
+        public static string GetVersion()
+        {
+            var v = Assembly.GetExecutingAssembly().GetName().Version;
+            return $"v{v.Major}.{v.Minor}";
+        }
+    }
+
+    public static class FileHelper
+    {
+        public static string ReadEmbeddedResource(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            using (StreamReader reader = new StreamReader(stream))
+            {
+                return reader.ReadToEnd();
+            }
+        }
+
+        public static byte[] ReadEmbeddedResourceBytes(string resourceName)
+        {
+            var assembly = Assembly.GetExecutingAssembly();
+            using (Stream stream = assembly.GetManifestResourceStream(resourceName))
+            {
+                if (stream == null)
+                    throw new FileNotFoundException($"Resource '{resourceName}' not found.");
+
+                using (var ms = new MemoryStream())
+                {
+                    stream.CopyTo(ms);
+                    return ms.ToArray();
+                }
+            }
+        }
+
+    }
+}
